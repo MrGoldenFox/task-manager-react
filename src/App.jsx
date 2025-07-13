36d59c50a193ac366f84fdad7bd6e1f0fb0ca868
MrGoldenFox <mrgoldenfoxnear@gmail.com>
2025-07-13 14:09:08 -0700

    fix key

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Greeting } from "./components/Greeting";
 
 const supabase = createClient(
   "https://uzkjwjvxcyithriiynit.supabase.co",
-  "YOUR_PUBLIC_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6a2p3anZ4Y3lpdGhyaWl5bml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNjkyMDAsImV4cCI6MjA2Nzk0NTIwMH0.HuW7GoyZAzlCMvC_S_dwrkuWsdsW4INckRKGFQlDs60"
 );
 
 export default function App() {
