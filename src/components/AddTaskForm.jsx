5e75fc5f4b37db101e93930a217b190b17d62bbb
MrGoldenFox <mrgoldenfoxnear@gmail.com>
2025-07-15 12:28:53 -0700

    fix button

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -19,9 +19,9 @@ export function AddTaskForm({ setTasks, tasks }) {
   return (
     <section className="my-5">
       <label htmlFor="Add">Add next task</label>
-      <div className="relative">
+      <div className="flex w-full">
         <input
-          className="border p-2 px-3 w-[80%] rounded-tl-sm rounded-bl-sm border-[var(--primary)]/25 outline-0 dark:border-[var(--primary-bg)]/40 border-r-0"
+          className="border p-2 px-3 w-full rounded-tl-sm rounded-bl-sm border-[var(--primary)]/25 outline-0 dark:border-[var(--primary-bg)]/40 border-r-0"
           type="text"
           name="Add"
           id="Add"
@@ -34,7 +34,7 @@ export function AddTaskForm({ setTasks, tasks }) {
             }
           }}
         />
-        <button onClick={AddNewTask} className="absolute right-0 top-1/2 w-[20%] -translate-y-1/2 bg-[var(--accent)] py-2 px-4 rounded-tr-sm rounded-br-sm text-[var(--primary-bg)] border-[var(--primary)]/25 border border-l-0 hover:bg-[var(--accent-hover)]">Add New</button>
+        <button onClick={AddNewTask} className="w-24 bg-[var(--accent)] py-2 px-4 rounded-tr-sm rounded-br-sm text-[var(--primary-bg)] border-[var(--primary)]/25 border border-l-0 hover:bg-[var(--accent-hover)] text-nowrap">Add New</button>
       </div>
     </section>
   );
